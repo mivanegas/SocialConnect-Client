48e78e5fad5d444302a65d358aebebdeebf53dcb
Maria Vanegas <marisavaneg@hotmail.com>
2026-03-17 13:48:44 -0700

    update

--- src/components/Posts.jsx
+++ src/components/Posts.jsx
@@ -1,20 +1,49 @@
+import { useEffect, useState } from "react";
 import Button from "react-bootstrap/Button";
 import Card from "react-bootstrap/Card";
+import ListGroup from "react-bootstrap/ListGroup";
+import axios from "axios";
 
-function Post({ currentUser }) {
+function Post({ post }) {
+  const [comments, setComments] = useState([]);
+
+  useEffect(() => {
+    fetchComments();
+  }, []);
+
+  async function fetchComments() {
+    try {
+      const API_URL = import.meta.env.VITE_API_URL;
+      const res = await axios.get(`${API_URL}/comments?post=${post._id}`);
+      setComments(res.data.data);
+    } catch (error) {
+      alert("Could not fetch comments");
+      console.log(error);
+    }
+  }
+
+  const { author } = post;
+
   return (
-    <Card className="mb-3">
-      <Card.Header>{currentUser.username} · just now</Card.Header>
-      <Card.Body>
-        <Card.Text>
-          Loving the sunny weather today! ☀️ Perfect day for a walk in the park.
-        </Card.Text>
-        <Button variant="outline-primary" size="sm" className="me-2">
-          👍 Like
+    <Card className="mt-2">
+      <Card.Header as="h5">
+        @{author.username} ({author.fullName})
+      </Card.Header>
+      <Card.Body className="m-3 p-2">
+        <Card.Text>{post.content}</Card.Text>
+        <Button variant="primary" size="sm" className="me-2 mb-2">
+          Like 👍
         </Button>
-        <Button variant="outline-secondary" size="sm">
-          💬 Comment
+        <Button variant="secondary" size="sm" className="mb-2">
+          Comment 💬
         </Button>
+        <ListGroup variant="flush">
+          {comments.map((c) => (
+            <ListGroup.Item key={c._id}>
+              @{c.author.username}: {c.content}
+            </ListGroup.Item>
+          ))}
+        </ListGroup>
       </Card.Body>
     </Card>
   );
--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,10 +1,31 @@
+import { useEffect, useState } from "react";
 import Post from "../components/Posts";
+import axios from "axios";
 
-function Landing({ currentUser }) {
+function Landing() {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    fetchPosts();
+  }, []);
+
+  async function fetchPosts() {
+    try {
+      const API_URL = import.meta.env.VITE_API_URL;
+      const res = await axios.get(`${API_URL}/posts`);
+      setPosts(res.data.data);
+    } catch (error) {
+      alert("Could not fetch posts");
+      console.log(error);
+    }
+  }
+
   return (
     <div>
       <h1 className="display-4">Posts</h1>
-      <Post currentUser={currentUser} />
+      {posts.map((p) => (
+        <Post key={p._id} post={p} />
+      ))}
     </div>
   );
 }
